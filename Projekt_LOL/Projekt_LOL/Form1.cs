a28dbdf1f9cb58be97a52de88f52ecad41ee7810
krzysztofambroziak <projekt2inzoprogramowania@gmail.com>
2015-03-08 22:39:46 +0100

    Punkt startowy

--- Projekt_LOL/Projekt_LOL/Form1.cs
+++ Projekt_LOL/Projekt_LOL/Form1.cs
@@ -22,14 +22,14 @@ namespace Projekt_LOL
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            string content = client.DownloadString("https://eune.api.pvp.net/api/lol/eune/v1.4/summoner/by-name/Klekot56,Mitronus,Hablarox?api_key=f4d10937-bd33-42ac-80ef-62290e4755bf");
-            richTextBox1.Text = richTextBox1.Text + content;
-            Dictionary<string, GraczJson> ListaGraczy = JsonConvert.DeserializeObject<Dictionary<string, GraczJson>>(content);
+            //string content = client.DownloadString("https://eune.api.pvp.net/api/lol/eune/v1.4/summoner/by-name/Klekot56,Mitronus,Hablarox?api_key=f4d10937-bd33-42ac-80ef-62290e4755bf");
+           // richTextBox1.Text = richTextBox1.Text + content;
+//Dictionary<string, GraczJson> ListaGraczy = JsonConvert.DeserializeObject<Dictionary<string, GraczJson>>(content);
 
         
-            string content2 = client.DownloadString("https://eune.api.pvp.net/api/lol/eune/v1.3/game/by-summoner/47719350/recent?api_key=f4d10937-bd33-42ac-80ef-62290e4755bf");
-            ListaGierJson ostatnieGry = JsonConvert.DeserializeObject<ListaGierJson>(content2);
-            richTextBox1.Text = richTextBox1.Text +"\n"+ content2;        
+          //  string content2 = client.DownloadString("https://eune.api.pvp.net/api/lol/eune/v1.3/game/by-summoner/47719350/recent?api_key=f4d10937-bd33-42ac-80ef-62290e4755bf");
+          //  ListaGierJson ostatnieGry = JsonConvert.DeserializeObject<ListaGierJson>(content2);
+          //  richTextBox1.Text = richTextBox1.Text +"\n"+ content2;        
         }
     }
 }
